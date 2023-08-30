9521086349e8d3c786a2c3cbb00a0511492cae2a
Hmtgit7 <hmtloharcoding3579@gmail.com>
2023-08-30 12:59:57 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import 'style.scss'
+
+import { Login, Register, Single, Write, Home } from 'pages'
+
+import { Navbar, Footer } from 'components';
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  )
+}
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />
+        },
+        {
+          path: "posts:id",
+          element: <Single />
+        },
+        {
+          path: "/write",
+          element: <Write />
+        },
+      ]
+    },
+    {
+      path: "/register",
+      element: <Register />
+    },
+    {
+      path: "/login",
+      element: <Login />
+    }
+  ]
+)
+
+const App = () => {
+  return (
+    <div className='app'>
+      <div className='container'>
+        <RouterProvider router={router} />
+      </div>
+    </div>
+  )
+}
+
+
+
+export default App
---
+++ client/src/pages/index.js
@@ -0,0 +1,6 @@
+export {default as About } from './Home';
+export {default as Login } from './Login';;
+export {default as Register } from './Register';
+export {default as Write } from './Write';
+export {default as Single } from './Single';
+export {default as Home } from './Home';
