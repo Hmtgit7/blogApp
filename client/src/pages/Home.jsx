bf4c356e59ab3b15e953c91bd2b9b8b3be0f61d9
Hmtgit7 <hmtloharcoding3579@gmail.com>
2023-09-15 19:10:36 +0530

    home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,55 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import Image from "../assests/image.jpeg"
 
 const Home = () => {
+  const posts = [
+    {
+      id: 1,
+      title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+      img:`${Image}`,
+    },
+    {
+      id: 2,
+      title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+      img: `${Image}`,
+    },
+    {
+      id: 3,
+      title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+      img: `${Image}`,
+    },
+    {
+      id: 4,
+      title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+      img: `${Image}`,
+    },
+  ];
   return (
-    <div>Home</div>
+    <div className='home'>
+      <div className="posts">
+        {posts.map(post => (
+          <div className="post" key={post.id}>
+            <div className="img">
+              <img src={post.img} alt="image is loading" />
+            </div>
+            <div className="content">
+              <Link className='link' to={`/post/${post.id}`}>
+                <h1>{post.title}</h1>
+                <p>{post.desc}</p>
+              </Link>
+             <button>Read More</button>
+            </div>
+
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
-export default Home
+export default Home;
