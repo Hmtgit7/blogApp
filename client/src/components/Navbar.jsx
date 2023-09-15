a2e474dd8e05fd8683ab2910fa31c425bd8507e9
Hmtgit7 <hmtloharcoding3579@gmail.com>
2023-09-15 19:10:49 +0530

    login page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,36 @@
 import React from 'react'
+import Logo from '../assests/logo-removebg-preview.png'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
       <div className='container'>
-        <div className='logo'>logo</div>
-        <div className='links'>links</div>
+        <div className='logo'>
+          <img src={Logo} alt="" />
+        </div>
+        <div className='links'>
+          <Link className='link' to="/?cat=art">
+            <h6>ART</h6>
+          </Link>
+          <Link className='link' to="/?cat=science">  
+            <h6>SCIENCE</h6>
+          </Link>
+          <Link className='link' to="/?cat=cinema">
+            <h6>CINEMA</h6>
+          </Link>
+          <Link className='link' to="/?cat=design">
+            <h6>DESIGN</h6>
+          </Link>
+          <Link className='link' to="/?cat=food">
+            <h6>FOOD</h6>
+          </Link>
+          <span>Hemant</span>
+          <span>Logout</span>
+          <span className='write'>
+            <Link to="/write"> Write</Link>
+          </span>
+        </div>
       </div>
     </div>
   )
