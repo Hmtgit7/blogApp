9d89657ec892ade7436c127ca15e07c862601962
Hmtgit7 <hmtloharcoding3579@gmail.com>
2023-09-15 19:09:16 +0530

    footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Logo from "../assests/logo-removebg-preview.png"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <img src={Logo} alt="footer" />
+      <span>Copyright @2023 made with ❤️ spark_developiee</span>
+    </footer>
   )
 }
 
-export default Footer
+export default Footer;
