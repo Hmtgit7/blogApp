edc60e4f154285b4231bc6fd9ae2030991be5884
Hmtgit7 <hmtloharcoding3579@gmail.com>
2023-09-15 19:09:35 +0530

    side menu

---
+++ client/src/components/Menu.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import Logo from "../assests/img.jpg"
+
+const Menu = () => {
+    const posts = [
+        {
+          id: 1,
+          title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+          desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+          img:`${Logo}`,
+        },
+        {
+          id: 2,
+          title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+          desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+          img: `${Logo}`,
+        },
+        {
+          id: 3,
+          title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+          desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+          img: `${Logo}`,
+        },
+        {
+          id: 4,
+          title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+          desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+          img: `${Logo}`,
+        },
+        {
+            id: 5,
+            title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+            desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+            img: `${Logo}`,
+          },
+      ];
+  return (
+    <div className='menu'>
+        <h1>Other posts you may like</h1>
+        {posts.map((post)=>{
+            return(
+                <div className="post" key={post.id}>
+                    <img src={post.img} alt="" />
+                    <h2>{post.title}</h2>
+                    <button>Read More</button>
+                </div>
+            )
+        })}
+    </div>
+  )
+}
+
+export default Menu
